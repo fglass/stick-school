811c875d4822e8b8cdb17f6439092af28f1ecb84
Fred Glass <glasshcs@gmail.com>
2021-08-16 23:33:26 +0100

    Introduce tabs to main menu

--- Assets/Scripts/Game/UI/MainMenu.cs
+++ Assets/Scripts/Game/UI/MainMenu.cs
@@ -8,19 +8,32 @@ namespace Game.UI
 {
     public class MainMenu : MonoBehaviour
     {
+        private static readonly Color Red = new Color(0.8588236f, 0.2235294f, 0.3098039f);
         [SerializeField] private Transform scenarioButtonPrefab;
+       
         private GameObject _mainMenu;
         private GameObject _playPanel;
+        
+        private TextMeshProUGUI _playText;
+        private TextMeshProUGUI _statsText;
+        private TextMeshProUGUI _settingsText;
 
         public void Awake()
         {
             _mainMenu = transform.Find("MainMenu").gameObject;
             _playPanel = _mainMenu.transform.Find("PlayPanel").gameObject;
+
+            var navBar = _mainMenu.transform.Find("NavBar");
+            _playText = navBar.Find("PlayButton").Find("Text").GetComponent<TextMeshProUGUI>();
+            _statsText = navBar.Find("StatsButton").Find("Text").GetComponent<TextMeshProUGUI>();
+            _settingsText = navBar.Find("SettingsButton").Find("Text").GetComponent<TextMeshProUGUI>();
+            
+            OnPlay();
         }
 
         public void CreateScenarioButtons(IEnumerable<Scenario.Scenario> scenarios)
         {
-            var xOffset = -400;
+            var xOffset = -550;
             
             foreach (var scenario in scenarios)
             {
@@ -29,27 +42,30 @@ namespace Game.UI
                 button.Translate(xOffset, 0, 0);
                 
                 button.Find("ScenarioName").GetComponent<TextMeshProUGUI>().text = scenario.Name.ToUpper();
-                button.GetComponent<Button>().onClick.AddListener(delegate { SelectScenario(scenario); });
+                button.GetComponent<Button>().onClick.AddListener(delegate { OnScenario(scenario); });
                 
-                xOffset += 450;
+                xOffset += 550;
             }
         }
 
         public void OnPlay()
         {
-            // TODO: tab selecting
+            DeselectTabs();
+            _playText.color = Red;
             _playPanel.SetActive(true);
         }
 
         public void OnStats()
         {
-            _playPanel.SetActive(false);
+            DeselectTabs();
+            _statsText.color = Red;
             Debug.Log("Open stats");
         }
 
         public void OnSettings()
         {
-            _playPanel.SetActive(false);
+            DeselectTabs();
+            _settingsText.color = Red;
             Debug.Log("Open settings");
         }
 
@@ -59,7 +75,15 @@ namespace Game.UI
             Application.Quit();
         }
         
-        private void SelectScenario(Scenario.Scenario scenario)
+        private void DeselectTabs()
+        {
+            _playText.color = Color.white;
+            _statsText.color = Color.white;
+            _settingsText.color = Color.white;
+            _playPanel.SetActive(false);
+        }
+        
+        private void OnScenario(Scenario.Scenario scenario)
         {
             _mainMenu.SetActive(false);
             EventBus.PublishPlay(scenario);
