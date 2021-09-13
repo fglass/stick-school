a5837ba7951706cabce3ebafef5fd4d0768f30c7
Fred Glass <glasshcs@gmail.com>
2021-09-13 23:47:24 +0100

    Rotate wireframe slowly

---
+++ Assets/Scripts/Environment/WireframeController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Environment
+{
+    public class WireframeController : MonoBehaviour
+    {
+        [SerializeField] private float rotationSpeed = 1f;
+        
+        public void Update()
+        {
+            transform.Rotate(0, 0, Time.deltaTime * rotationSpeed, Space.World);
+        }
+    }
+}
